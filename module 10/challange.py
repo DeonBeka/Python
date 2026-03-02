3494029f5c78aed3d9d71020704ff10bb412c779
DeonBeka <deonbeka92@gmail.com>
2026-03-02 19:17:18 +0100

    ./.

---
+++ module 10/challange.py
@@ -0,0 +1,77 @@
+
+class DigitalSchool:
+    def __init__(self, name, city, state, courses):
+        self.__name = name
+        self.__city = city
+        self.__state = state
+        self.__courses = courses
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, value):
+        self.__city = value
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, value):
+        self.__state = value
+
+    @property
+    def courses(self):
+        return self.__courses
+
+    @courses.setter
+    def courses(self, value):
+        self.__courses = value
+
+    def show_school_info(self):
+        return {
+            "name": self.__name,
+            "city": self.__city,
+            "state": self.__state,
+            "courses": self.__courses
+        }
+
+    def organize_hackathon(self):
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+class DS_Prishtina(DigitalSchool):
+    def __init__(self, name, city, state, courses, student_number):
+        super().__init__(name, city, state, courses)
+        self.__student_number = student_number
+
+    @property
+    def student_number(self):
+        return self.__student_number
+
+    def SCF(self):
+        print("DS Prishtina is organizing the Spring Code Fest (SCF)!")
+
+    def organize_hackathon(self):
+        print("DS Prishtina is organizing a city-wide hackathon for students!")
+
+ds_prishtina = DS_Prishtina(
+    name="Digital School Prishtina",
+    city="Prishtina",
+    state="Kosovo",
+    courses=["Python", "Web Development", "AI"],
+    student_number=450
+)
+ds_prishtina.SCF()
+ds_prishtina.organize_hackathon()
+
+print(f"The number of students in DS_Prishtina is {ds_prishtina.student_number}")
