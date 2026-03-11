f03c5ed009af73d9b0091bead14f5817164b445d
DeonBeka <deonbeka92@gmail.com>
2026-03-11 18:42:03 +0100

    ,.,

--- challange/Bmiapp.py
+++ challange/Bmiapp.py
@@ -16,7 +16,7 @@ class Person(ABC):
             raise ValueError("Weight cannot be negative")
         self._weight = value
 
-    @propery
+    @property
     def height(self):
         return self._height
     @height.setter
@@ -33,3 +33,62 @@ class Person(ABC):
     def print_info(self):
         print(f"{self.name}, age:{self.age}, weight:{self.weight} kg, height:{self.height},"
               f"BMI{self.calculate_bmi():.2f}, category:{self.get_bmi_category()}")
+
+class Adult(Person):
+    def calculate_bmi(self):
+        return (self.weight / (self.height ** 2))
+
+    def get_bmi_category(self):
+        bmi = self.calculate_bmi()
+        if bmi < 18.5:
+            return "Underweight"
+        elif 18.5 <= bmi < 24.9:
+            return "Normal weight"
+        elif 24.9 <= bmi < 29.9:
+            return "Overweight"
+        else:
+            return "Obese"
+
+class Child(Person):
+    def calculate_bmi(self):
+        return (self.weight / (self.height ** 2)) * 1.3
+
+    def get_bmi_category(self):
+        bmi = self.calculate_bmi()
+        if bmi < 14:
+            return "Underweight"
+        elif 14 <= bmi < 18:
+            return "Normal weight"
+        elif 18 <= bmi < 24:
+            return "Overweight"
+        else:
+            return "Obese"
+
+class BMIApp:
+    def __init__(self):
+        self.people = []
+    def add_person(self, person):
+        self.people.append(person)
+    def collect_user_data(self):
+        name = input("Enter name:")
+        age = int(input("Enter age:"))
+        weight = float(input("Enter weight in kilograms:"))
+        height = float(input("Enter height in meters:"))
+        if age>+18:
+            person = Adult(name, age , weight, height)
+        else:
+            person = Child(name, age, weight,height)
+        self.add_person(person)
+    def print_results(self):
+        for person in self.people:
+            person.print_info()
+    def run(self):
+        while True:
+            self.collect_user_data()
+            cont = input("Do you want to add another person?(yes/no)").strip().lower()
+            if cont != 'yes':
+                break
+        self.print_results()
+
+bmi = BMIApp()
+bmi.run()
