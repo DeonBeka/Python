8e80628072d70da461d66fab9227841fb78c8618
DeonBeka <deonbeka92@gmail.com>
2026-04-15 19:17:53 +0200

    ,.,

--- module19/main.py
+++ module19/main.py
@@ -17,3 +17,57 @@ with st.sidebar.form("book_form"):
     new_year = st.number_input("Year", min_value= 2009, max_value=2022, step= 1)
     new_genre = st.selectbox("Genre", books_df['Genre'].unique())
     submit_button = st.form_submit_button(label="Add Book")
+
+if submit_button:
+    new_data = {
+        "Name": new_name,
+        "Author":new_author,
+        "User Rating": new_user_rating
+        "Reviews":new_reviews,
+        "Price":new_price,
+        "Year": new_year,
+        "Genre":new_genre
+    }
+    books_df = pd.concat([pd.DataFrame(new_data, index=[0]), books_df], ignore_index=True)
+    books_df.to_csv('bestsellers_with_categories_2022_03_27.csv', index=False)
+    st.sidebar.success("New book added successfully!")
+
+
+st.subheader("Summary Statistics")
+total_books = books_df.shape[0]
+unique_titles = books_df['Name'].unique()
+average_rating = books_df["User Rating"].mean()
+average_price = books_df["Price"].mean()
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total Books", total_books)
+col2.metric("Unique Titles", unique_titles)
+col3.metric("Average Rating", f"{average_rating:.2f}")
+col4.metric("Average Price", f"{average_rating:.2f}")
+
+st.subheader("Dataset Preview")
+st.write(books_df.head())
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Top 10 Book Titles")
+    top_authors = books_df['Name'].value_counts().head(10)
+    st.bar_chart(top_titles)
+
+with col2:
+    st.subheader("Top 10 Authors")
+    top_authors = books_df['Author'].value_counts().head(10)
+    st.bar_chart(top_authors)
+
+st.subheader("Genre Distribution")
+fig = px.pie(books_df, names="Genre", titles='Most Liked Genre(2009-2022)', color="Genre",
+             color_discrete_sequence=px.colors.sequential.Plasma)
+st.plotly_chart(fig)
+
+st.subheader("Number of Fiction vs Non-Fiction Books Over the Years")
+size = books_df.groupby(['Year', 'Genre']),size().reset_index(name='Counts')
+fig = px.bar(size, x='Year', y='Counts', color="Genre", title='Number of Fiction vs Non-Fiction from 2009-2022',
+             color_discrete_sequence=px.colors.sequential.Plasma, barmode='group')
+st.plotly_chart(fig)
+
